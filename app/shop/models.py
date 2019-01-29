57854273cebe79a4262ac516bb47a7cf8c24cd14
HyungtaeMoon <blessmht@gmail.com>
2019-01-29 21:39:56 +0900

    [COMMENT] shop.models 의 필드에 대한 주석 추가

--- app/shop/models.py
+++ app/shop/models.py
@@ -3,8 +3,12 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
+    # 글자, 숫자, 밑줄, 하이픈만 취급.
+    #   url 주소에 대문자, 공백이 들어갈 경우에 %20 과 같은 문자가 들어가게 되어
+    #   명확한 url 주소가 아님.
     slug = models.SlugField(max_length=250, unique=True)
     description = models.TextField(blank=True)
+    # media/category 경로에 이미지가 저장됨
     image = models.ImageField(upload_to='category', blank=True)
 
     class Meta:
@@ -21,9 +25,11 @@ class Product(models.Model):
     slug = models.SlugField(max_length=250, unique=True)
     description = models.TextField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # 소수점까지 표현할 수 있는 Decimafield 를 사용
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='product', blank=True)
     stock = models.IntegerField()
+    # 체크박스 형태로 True/False 를 가짐
     available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
