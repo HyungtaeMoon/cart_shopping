a636e1dd6d9b2d4d13cdc14adcc4d3d5638c53e1
HyungtaeMoon <blessmht@gmail.com>
2019-01-29 20:55:23 +0900

    [ADD] static, media root 설정

--- app/config/urls.py
+++ app/config/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from config import settings
 from shop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
 ]
+if settings.DEBUG:
+    urlpatterns += static(prefix=settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(prefix=settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
