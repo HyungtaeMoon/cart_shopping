1b25f22ab470649e8e4d896612c0d5dc571121c4
HyungtaeMoon <blessmht@gmail.com>
2019-02-05 19:44:06 +0900

    [ADD] paginator 기능 구현

--- app/shop/views.py
+++ app/shop/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
@@ -17,20 +18,38 @@ def allProdCat(request, c_slug=None):
     """
     # category 페이지
     c_page = None
-    products = None
+    products_list = None
+
     # category 의 slugField 가 None 이 아닌 경우에
     if c_slug!=None:
-        # Product 객체를 만들고, url 을 통해 들어온 slug 필드를 Category.slugField 에 매핑
+        # Product 객체에 접근하고, url 을 통해 들어온 slug 필드를 Category.slugField 에 매핑
         c_page = get_object_or_404(Category, slug=c_slug)
         # DB 에 접근(조건은 c_page: 상품에 해당하는 모든 카테고리, available 이 True 값)
-        products = Product.objects.filter(category=c_page, available=True)
+        products_list = Product.objects.filter(category=c_page, available=True)
     else:
         # c_slug 가 None: url 에서 slug 를 입력하지 않고 접근할 경우,
         #  available 가 True 인 모든 상품 페이지를 보여줌
-        products = Product.objects.all().filter(available=True)
+        products_list = Product.objects.all().filter(available=True)
+
+    # Paginator(object_list, per_page)
+    paginator = Paginator(products_list, 6)
+    try:
+        # paginator 로 몇번째의 페이지로 get 요청을 최초로 보낼지 설정
+        #   ('2'로 설정하면 최초 get 요청으로 보여지는 페이지는 2페이지로 설정됨)
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+    try:
+        # 위의 try, except 문에서 설정한 page 변수를 사용하여 products 의 페이지 설정
+        #   paginator.page 에서 page 는 Paginator 의 메서드, (page) 는 위에서 변수로 지정한 page
+        products = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        print(f'?page={page} 는 존재하지 않는 페이지로 2페이지에서 url get 요청 또는 str 입력시 2페이지로 다시 redirect 됩니다')
+        products = paginator.page(paginator.num_pages)
+
     # context 에 c_page 가 있으면 category 와 매핑하여 보여주고
     #   없으면 이용가능한 모든 상품을 렌더 시켜줌
-    return render(request, 'shop/category.html', {'category': c_page, 'products': products})
+    return render(request, 'shop/category.html', {'category': c_slug, 'products': products})
 
 
 def product_cat_detail(request, c_slug, product_slug):
