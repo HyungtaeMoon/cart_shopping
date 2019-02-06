06c98467df8b31dc6d22306c45cc0e0e8ea488a1
HyungtaeMoon <blessmht@gmail.com>
2019-02-06 16:42:27 +0900

    [ADD] cart.models.Cart, CartItem 모델 생성

--- app/cart/models.py
+++ app/cart/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from shop.models import Product
+
+
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=250, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'Cart'
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.cart_id
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'CartItem'
+
+    def sub_total(self):
+        self.product.price = self.quantity
+
+    def __str__(self):
+        return self.product
