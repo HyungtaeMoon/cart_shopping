c5249dd8b4c699c32ed76c8ff58b0c6e60057bcd
HyungtaeMoon <blessmht@gmail.com>
2019-02-08 20:08:54 +0900

    [ADD] order.models.Order모델, OrderItem 모델 생성 및 migrate

--- app/order/models.py
+++ app/order/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    token = models.CharField(max_length=250, blank=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='USD Order total')
+    emailAddress = models.EmailField(max_length=250, blank=True, verbose_name='Email Address')
+    created_at = models.DateTimeField(auto_now_add=True)
+    billingName = models.CharField(max_length=250, blank=True)
+    billingAddress1 = models.CharField(max_length=250, blank=True)
+    billingCity = models.CharField(max_length=250, blank=True)
+    billingPostcode = models.CharField(max_length=10, blank=True)
+    billingCountry = models.CharField(max_length=200, blank=True)
+    shippingName = models.CharField(max_length=250, blank=True)
+    shippingAddress1 = models.CharField(max_length=250, blank=True)
+    shippingCity = models.CharField(max_length=250, blank=True)
+    shippingPostcode = models.CharField(max_length=10, blank=True)
+    shippingCountry = models.CharField(max_length=200, blank=True)
+
+    class Meta:
+        db_table = 'Order'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    product = models.CharField(max_length=250, blank=True)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='USD price')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'OrderItem'
+
+    def sub_total(self):
+        return self.quantity * self.price
+
+    def __str__(self):
+        return self.product
