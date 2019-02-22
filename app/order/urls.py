9bb1b0ba9a478aa25d93649caa06e952aa8cf816
HyungtaeMoon <blessmht@gmail.com>
2019-02-22 12:51:38 +0900

    [ADD] order.order_history, view_order 기능 구현

--- app/order/urls.py
+++ app/order/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'order'
 
 urlpatterns = [
-    path('thanks/<int:order_id>/', views.thanks, name='thanks')
+    path('thanks/<int:order_id>/', views.thanks, name='thanks'),
+    path('history/', views.order_history, name='order_history'),
+    path('<int:order_id>/', views.view_order, name='order_detail'),
 ]
--- app/order/views.py
+++ app/order/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-from .models import Order
+from .models import Order, OrderItem
 
 
 def thanks(request, order_id):
@@ -10,3 +11,27 @@ def thanks(request, order_id):
     if order_id:
         customer_order = get_object_or_404(Order, id=order_id)
         return render(request, 'thanks.html', {'customer_order': customer_order})
+
+
+@login_required
+def order_history(request):
+    if request.user.is_authenticated:
+        email = str(request.user.email)
+        order_details = Order.objects.filter(emailAddress=email)
+        context = {
+            'order_details': order_details,
+        }
+    return render(request, 'order/orders_list.html', context)
+
+
+@login_required
+def view_order(request, order_id):
+    if request.user.is_authenticated:
+        email = str(request.user.email)
+        order = Order.objects.get(id=order_id, emailAddress=email)
+        order_items = OrderItem.objects.filter(order=order)
+        context = {
+            'order': order,
+            'order_items': order_items,
+        }
+    return render(request, 'order/order_detail.html', context)
